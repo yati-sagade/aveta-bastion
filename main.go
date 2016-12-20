9c1a12a4563f088c034c8db5e243b3fdb552b91f
Yati Sagade <yati.sagade@gmail.com>
2016-12-20 23:25:55 +0100

    Change image frame message protocol. Now the same handler can handle multiple message types.

--- main.go
+++ main.go
@@ -3,4 +3,5 @@ package main
 func main() {
 	srv := NewDataCollectionServer("0.0.0.0:9000")
 	srv.Start()
+	//StartTimetampReceiver()
 }
--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"io"
 	"log"
+	"math"
 	"net"
 	"os/exec"
 )
@@ -22,7 +24,6 @@ func (srv *DataCollectionServer) Start() error {
 		return err
 	}
 	defer l.Close()
-	log.Println("Listening for connections on " + srv.Addr)
 	for {
 		conn, err := l.Accept()
 		if err != nil {
@@ -32,35 +33,53 @@ func (srv *DataCollectionServer) Start() error {
 	}
 }
 
+type timestampedBytes struct {
+	timestamp float64
+	data      []byte
+}
+
 func (srv *DataCollectionServer) HandleConnection(c net.Conn) {
-	var imgSize int32
+	var msgHeader struct {
+		Flags         uint8
+		TimeStampBits uint64
+		ImgSize       uint32
+	}
 	q := make(chan struct{})
-	data := make(chan []byte)
+	data := make(chan timestampedBytes)
+
 	go VideoWriter(data, q)
+	defer func() {
+		q <- struct{}{}
+	}()
+
 IMAGE:
 	for {
-		err := binary.Read(c, binary.LittleEndian, &imgSize)
+		err := binary.Read(c, binary.LittleEndian, &msgHeader)
 		if err != nil {
-			log.Fatalf("Error reading image size: " + err.Error())
+			if err == io.EOF {
+				break IMAGE
+			}
+			log.Fatalf("Error reading image header: " + err.Error())
 		}
-		if imgSize == 0 {
-			break IMAGE
+		if msgHeader.Flags&0x80 == 0x80 {
+			// End of stream
+			return
 		}
-		imgBuff := make([]byte, imgSize)
+		timeStamp := math.Float64frombits(msgHeader.TimeStampBits)
+		imgBuff := make([]byte, msgHeader.ImgSize)
 		readSoFar := 0
-		for readSoFar != int(imgSize) {
-			n, err := c.Read(imgBuff[readSoFar:imgSize])
+		for readSoFar != int(msgHeader.ImgSize) {
+			n, err := c.Read(imgBuff[readSoFar:msgHeader.ImgSize])
 			if err != nil {
 				log.Fatalf("Error reading image data: " + err.Error())
 			}
 			readSoFar += n
 		}
-		data <- imgBuff
+		data <- timestampedBytes{timeStamp, imgBuff}
 	}
-	q <- struct{}{}
 }
 
-func VideoWriter(data chan []byte, quit chan struct{}) {
+func VideoWriter(data chan timestampedBytes, quit chan struct{}) {
 	drawTextFilter := `vflip, fps=10, drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf: text='%{localtime\:%s}': fontcolor=white@1.0: x=100: y=100`
 	command := exec.Command("/usr/bin/ffmpeg",
 		"-f", "mjpeg",
@@ -100,7 +119,7 @@ IMAGE:
 	for {
 		select {
 		case imgBytes := <-data:
-			_, err = inPipe.Write(imgBytes)
+			_, err = inPipe.Write(imgBytes.data)
 			if err != nil {
 				buf := new(bytes.Buffer)
 				buf.ReadFrom(errPipe)
